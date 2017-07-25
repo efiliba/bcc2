c97ab943f385065742619388b660a0de596f0b86
Eli Filiba <efiliba@hotmail.com>
2017-07-25 18:43:20 +1000

    Flexbox inner content background color height consistent.

--- src/aged_care/ServicesWeProvideTiles.js
+++ src/aged_care/ServicesWeProvideTiles.js
@@ -3,15 +3,24 @@ import './ServicesWeProvideTiles.css';
 import { Link } from 'react-router-dom';
 
 const Tile = ({tile}) =>
-  <Link className="flex-item" to={tile.link}>
-    <div className="service-tile__heading">
-      {tile.heading}
+  <div className="flex-item">
+    <div className="flex-item__container">
+      <div className="service-tile__heading">
+        {tile.heading}
+      </div>
+      <div className="flex-item__container--content">
+        <Link className="service-tile__content" to={tile.link}>
+          <div className="service-tile__image">
+            <img src={`../images/${tile.image}`} alt={tile.heading} />
+          </div>
+          <div className="service-tile__description">
+            {tile.description}
+          </div>
+          <div className="service-tile__more">MORE</div>
+        </Link>
+      </div>
     </div>
-    <div className="service-tile__image">
-      <img src={`../images/${tile.image}`} alt={tile.heading} />
-    </div>
-    <div className="service-tile__description">{tile.description}</div>
-  </Link>;
+  </div>;
 
 export const ServicesWeProvideTiles = ({tiles}) =>
   <div className="service-tiles flex-container">
